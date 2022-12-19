bfa7f853c3570d9914094068b499c4d0ecef4cb8
aleks-g <aleksgro@math.uio.no>
2022-12-19 09:09:39 +0000

    Add overriding of component attrs
    There are more attributes in pypsa-eur-sec;
    need to override those used in the pypsa-eur module;
    this is done 1x in the Snakefile (but needs to be done much more often);
    stole a function and pasted into compute_optimum.py

--- workflow/scripts/compute_optimum.py
+++ workflow/scripts/compute_optimum.py
@@ -12,13 +12,44 @@ import pypsa
 from _helpers import configure_logging
 from pypsa.linopf import ilopf, network_lopf
 from utilities import get_basis_values
+from pypsa.components import component_attrs, components
+import os.path
+
+# This function is stolen from pypsa-eur-sec/scripts/helper.py:
+def override_component_attrs(directory):
+    """Tell PyPSA that links can have multiple outputs by
+    overriding the component_attrs. This can be done for
+    as many buses as you need with format busi for i = 2,3,4,5,....
+    See https://pypsa.org/doc/components.html#link-with-multiple-outputs-or-inputs
+
+    Parameters
+    ----------
+    directory : string
+        Folder where component attributes to override are stored 
+        analogous to ``pypsa/component_attrs``, e.g. `links.csv`.
+
+    Returns
+    -------
+    Dictionary of overriden component attributes.
+    """
+
+    attrs = dict({k : v.copy() for k,v in component_attrs.items()})
+
+    for component, list_name in components.list_name.items():
+        fn = f"{directory}/{list_name}.csv"
+        if os.path.isfile(fn):
+            overrides = pd.read_csv(fn, index_col=0, na_values="n/a")
+            attrs[component] = overrides.combine_first(attrs[component])
+
+    return attrs
 
 if __name__ == "__main__":
     # Set up logging so that everything is written to the right log file.
     configure_logging(snakemake)
 
     # Load the network and solving options.
-    n = pypsa.Network(snakemake.input.network)
+    overrides = override_component_attrs(snakemake.input.overrides)
+    n = pypsa.Network(snakemake.input.network, override_component_attrs=overrides)
     solving_options = snakemake.config["pypsa-eur"]["solving"]["options"]
     solver_options = snakemake.config["pypsa-eur"]["solving"]["solver"].copy()
     solver_name = solver_options.pop("name")
